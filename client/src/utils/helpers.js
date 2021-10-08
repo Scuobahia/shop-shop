5931ed727a5816c303b0a7cd61ce194d38e6caf9
Scuobahia <antonio.sr8808@gmail.comgit config --global user.name Antonio>
2021-10-07 19:57:46 -0600

    reducers from the lesson 2 needs modification

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,67 +1,67 @@
 export function pluralize(name, count) {
   if (count === 1) {
-    return name
+    return name;
   }
-  return name + 's'
+  return name + "s";
 }
 
 export function idbPromise(storeName, method, object) {
-  return new Promise ((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     // open connection to shop-shop version 1 db
-    const request = window.indexedDB.open('shop-shop', 1);
+    const request = window.indexedDB.open("shop-shop", 1);
 
     let db, tx, store;
 
     // on version change or first accessing the website run function and create the appropriate objectStores
-    request.onupgradeneeded = function(e) {
+    request.onupgradeneeded = function (e) {
       const db = request.result;
 
-      db.createObjectStore('products', { keyPath: '_id'});
-      db.createObjectStore('categories', { keyPath: '_id'});
-      db.createObjectStore('cart', { keyPath: '_id'});
+      db.createObjectStore("products", { keyPath: "_id" });
+      db.createObjectStore("categories", { keyPath: "_id" });
+      db.createObjectStore("cart", { keyPath: "_id" });
     };
-    
+
     // Handle connection errors
     request.onerror = function (e) {
-      console.log('Uh oh! Something went wrong!')
-    }
+      console.log("Uh oh! Something went wrong!");
+    };
 
     request.onsuccess = function (e) {
       db = request.result;
 
       // open transaction and store whatever is passed to storeName
-      tx = db.transaction(storeName, 'readwrite');
-      // save reference to declared object store 
+      tx = db.transaction(storeName, "readwrite");
+      // save reference to declared object store
       store = tx.objectStore(storeName);
       // console.log(store)
       // console log if error
       db.onerror = function (e) {
-        console.log('Error:', e)
-      }
+        console.log("Error:", e);
+      };
 
-      switch(method) {
-        case 'put': 
-          console.log('put:', store.put(object))
+      switch (method) {
+        case "put":
+          console.log("put:", store.put(object));
           store.put(object);
           // console.log('resolution:', resolve(object))
           resolve(object);
           break;
-        case 'get': 
+        case "get":
           const all = store.getAll();
           all.onsuccess = function () {
             resolve(all.result);
-          }
+          };
           break;
-        case 'delete':
+        case "delete":
           store.delete(object._id);
           break;
         default:
-          console.log('No valid method')
+          console.log("No valid method");
           break;
       }
 
       tx.oncomplete = function () {
-        db.close()
+        db.close();
       };
     };
   });
